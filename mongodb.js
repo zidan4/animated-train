9754f8184e6b880546a4e0c6165e4e8d5d774a60
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-03-06 20:26:56 +0300

    Create mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+export const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("🔥 MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Error:", error);
+  }
+};
