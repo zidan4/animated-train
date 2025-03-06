c8a23fd9857a58a3f5e00c93555015fe3aed3f86
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-03-06 20:27:22 +0300

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema({
+  userId: String,
+  messages: [{ role: String, content: String }],
+});
+
+export default mongoose.models.Chat || mongoose.model("Chat", ChatSchema);
